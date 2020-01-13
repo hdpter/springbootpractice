396adb9c0f067d2b79f671fe672a85ec3a9d5198
李福春 <fuchun.li@lifesense.com>
2020-01-13 11:33:47 +0800

    定时任务

--- demo_spring_toher/src/test/java/com/springbootpractice/demo/spring/other/DemoSpringOtherApplicationTests.java
+++ demo_spring_toher/src/test/java/com/springbootpractice/demo/spring/other/DemoSpringOtherApplicationTests.java
@@ -1,12 +1,15 @@
 package com.springbootpractice.demo.spring.other;
 
 import com.springbootpractice.demo.spring.other.service.AsyncSummaryDataService;
+import com.springbootpractice.demo.spring.other.service.ScheduleTaskService;
 import com.springbootpractice.demo.spring.other.service.SummaryDataService;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class DemoSpringOtherApplicationTests {
@@ -16,6 +19,9 @@ class DemoSpringOtherApplicationTests {
 
     @Autowired
     private AsyncSummaryDataService asyncSummaryDataService;
+
+    @Autowired
+    private ScheduleTaskService scheduleTaskService;
 
     @Test
     void asyncTest() {
@@ -35,4 +41,19 @@ class DemoSpringOtherApplicationTests {
 
     }
 
+    @Test
+    void scheduleTest() {
+
+        scheduleTaskService.generateMonthReport();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        try {
+            TimeUnit.MINUTES.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
