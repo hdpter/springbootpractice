ca780e1101eb8629835a340cd62d766d733cb132
李福春 <fuchun.li@lifesense.com>
2020-01-16 10:51:15 +0800

    xxx

--- demo_webflux/src/main/java/com/springbootpractice/demo/webflux/controller/UserController.java
+++ demo_webflux/src/main/java/com/springbootpractice/demo/webflux/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.springbootpractice.demo.webflux.controller;
 
+import com.springbootpractice.demo.webflux.core.UserValidator;
 import com.springbootpractice.demo.webflux.dao.entity.User;
 import com.springbootpractice.demo.webflux.service.UserService;
+import org.springframework.validation.DataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
 
 /**
  * 说明：TODO
@@ -31,4 +36,14 @@ public class UserController {
     public Flux<User> getUser(@PathVariable("userName") String userName, @PathVariable("note") String note){
         return userService.findByUserNameAndNote(userName,note);
     }
+
+    @GetMapping(path = "/user/insert/{user}")
+    public Mono<User> insertUser(@Valid  @PathVariable("user")  User user){
+        return userService.insertUser(user);
+    }
+
+//    @InitBinder
+//    public void initBinder(DataBinder binder){
+//        binder.setValidator(new UserValidator());
+//    }
 }
