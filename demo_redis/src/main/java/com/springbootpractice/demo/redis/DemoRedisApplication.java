49b0bb88619af9e2b1a7b303acacbb1958270642
李福春 <fuchun.li@lifesense.com>
2020-01-21 19:08:30 +0800

    redis demo

--- demo_redis/src/main/java/com/springbootpractice/demo/redis/DemoRedisApplication.java
+++ demo_redis/src/main/java/com/springbootpractice/demo/redis/DemoRedisApplication.java
@@ -1,9 +1,15 @@
 package com.springbootpractice.demo.redis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
+@MapperScan(annotationClass = Repository.class,
+        basePackages = "com.springbootpractice.demo.redis.dao.mapper")
+@EnableCaching
 public class DemoRedisApplication {
 
     public static void main(String[] args) {
