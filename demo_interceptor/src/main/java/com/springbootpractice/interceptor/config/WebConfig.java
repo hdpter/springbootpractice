c927a9a8a761ee47e1d6b9e29981db1c954bf349
李福春 <fuchun.li@lifesense.com>
2020-02-19 23:35:41 +0800

    拦截器例子

--- demo_interceptor/src/main/java/com/springbootpractice/interceptor/config/WebConfig.java
+++ demo_interceptor/src/main/java/com/springbootpractice/interceptor/config/WebConfig.java
@@ -1,10 +1,24 @@
 package com.springbootpractice.interceptor.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springbootpractice.interceptor.config.interceptor.MyInterceptor;
+import lombok.SneakyThrows;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 说明：TODO
@@ -12,11 +26,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * 创建时间： 2020年02月19日 11:03 下午
  **/
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+@ControllerAdvice
+public class WebConfig implements WebMvcConfigurer, ResponseBodyAdvice {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         HandlerInterceptor myIntercepter = new MyInterceptor() ;
         registry.addInterceptor(myIntercepter).addPathPatterns("/**");
+    }
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @SneakyThrows
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+
+       Map<String,Object> map = new HashMap();
+       map.put("result","true");
+       map.put("data",body);
+       return new ObjectMapper().writeValueAsString(map);
     }
 }
