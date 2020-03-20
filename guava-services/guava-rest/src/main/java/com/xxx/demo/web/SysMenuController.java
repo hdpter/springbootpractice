8eb777fa69af87932a4a9a3b8636e33714635fc7
李福春 <fuchun.li@lifesense.com>
2020-03-20 09:31:54 +0800

    多模块模板m

--- guava-services/guava-rest/src/main/java/com/xxx/demo/web/SysMenuController.java
+++ guava-services/guava-rest/src/main/java/com/xxx/demo/web/SysMenuController.java
@@ -6,6 +6,8 @@ import com.xxx.demo.biz.service.SysMenuService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -13,36 +15,42 @@ import java.util.List;
 /**
 * Created by CodeGenerator on 2020/03/19.
 */
+@Api("SysMenu模块")
 @RestController
 @RequestMapping("/sys/menu")
 public class SysMenuController {
     @Resource
     private SysMenuService sysMenuService;
 
+    @ApiOperation("添加")
     @PostMapping
     public Result add(@RequestBody SysMenu sysMenu) {
         sysMenuService.save(sysMenu);
         return Result.genSuccessResult();
     }
 
+    @ApiOperation("删除")
     @DeleteMapping("/{id}")
     public Result delete(@PathVariable Integer id) {
         sysMenuService.deleteById(id);
         return Result.genSuccessResult();
     }
 
+    @ApiOperation("更新")
     @PutMapping
     public Result update(@RequestBody SysMenu sysMenu) {
         sysMenuService.update(sysMenu);
         return Result.genSuccessResult();
     }
 
+    @ApiOperation("查单个")
     @GetMapping("/{id}")
     public Result detail(@PathVariable Integer id) {
         SysMenu sysMenu = sysMenuService.findById(id);
         return Result.genSuccessResult(sysMenu);
     }
 
+    @ApiOperation("分页查询")
     @GetMapping
     public Result list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
         PageHelper.startPage(page, size);
